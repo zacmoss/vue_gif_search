4838b929031f48c3b8c7d95fa5537a3355b66bf9
Zac <zachary.moss@ymail.com>
2021-05-27 17:00:33 -0500

    Removed uneeded code and made style changes

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,26 @@
     <input @input="debounceSearch" v-model="search_string" placeholder="search gifs">
     <button @click="submit">Submit</button>
   </div>
-  <div v-if="this.error_message" class="error_message_container">
-    <div class="error_message">
-      <p><strong>{{ this.error_message }}</strong></p>
+  <div class="message_parent_container">
+    <div class="message_child_container">
+      <div v-if="this.error_message" class="error_message">
+        <p><strong>{{ this.error_message }}</strong></p>
+      </div>
+      <div v-else-if="this.show_copied_message" class="copied_message">
+        <p><strong>Copied to clipboard!</strong></p>
+      </div>
+      <div v-else-if="search_string">
+        <p><strong>Searching the interwebs for cool gifs matching: {{ search_string }}</strong></p>
+      </div>
     </div>
   </div>
-  
-  <div style="height: 20px">
-  <p v-if="search_string && !this.error_message"><strong>Searching the interwebs for cool gifs matching: {{ search_string }}</strong></p>
-  </div>
-  <div class="row">
+  <div class="gif_parent_container">
+  <div class="gif_container">
     <div class="loader" v-if="loading">
       <p>Loading...</p>
     </div>
-    <div v-else-if="this.display_gif" :class="test">
-      <div style="display:flex; justify-content: space-between; margin-bottom: 1rem">
+    <div v-else-if="this.display_gif">
+      <div class="cycle_container">
         <button @click="this.index > 0 ? cycle('-') : null">Previous</button>
         <button @click="this.index + 1 < this.displayed_gifs.length ? cycle('+') : null">Next</button>
       </div>
@@ -27,10 +32,16 @@
           <source :src="this.display_gif.images.original_mp4.mp4" type="video/mp4">
         </video>
       </div>
+      <div class="copy_to_clipboard_container">
+      <button @click="this.copyToClipboard">Copy To Clipboard</button>
+      </div>
     </div>
   </div>
+  </div>
+  <div class="footer_container">
   <div class="footer">
     <img src="./assets/giphy_logo.png">
+  </div>
   </div>
 </template>
 
@@ -50,20 +61,18 @@ export default {
       giphyURL: "",
       keys: [],
       stored_gifs: [],
-      test: "one_col",
       loading: false,
       debounce: null,
       too_many_requests: false,
       error_message: '',
-      index: 0
+      index: 0,
+      show_copied_message: false
     }
   },
   mounted: function() {
     // pull in env vars (must have VUE_APP_ prefix to work with webpack)
     this.giphy_api_key = process.env.VUE_APP_GIPHY_API_KEY
-
     this.getGifs()
-    
   },
   watch: {
     too_many_requests: function (val) {
@@ -71,11 +80,13 @@ export default {
       if (val) {
         this.error_message = "Api request limit has been reached."
       }
+    },
+    display_gif: function () {
+      this.show_copied_message = false
     }
   },
   methods: {
     submit: function () {
-      this.setRandom() // do we even have to set it here? it would have been set on keystroke stop
       this.getGifs()
     },
     formatSearch: function() {
@@ -97,6 +108,7 @@ export default {
       }, 600)
     },
     getGifs: function () {
+      this.show_copied_message = false
       this.loading = true
       this.formatSearch()
       if (this.random) {
@@ -104,9 +116,8 @@ export default {
       } else {
         axios.get(this.giphyURL)
         .then((response) => {
-          // TODO - Test this with test var
           if (response.data.meta.status === 200) {
-            this.displayed_gifs = [] // maybe make this a helper function that can be called multiple places
+            this.displayed_gifs = []
             for (let i = 0; i < response.data.data.length; i++) {
               this.storeData(response.data.data[i])
             }
@@ -146,21 +157,12 @@ export default {
         console.log('ERROR: Attempting to hit random gif url when not set to random')
       }
     },
-    requestGifs: function () {
-      axios.get(this.giphyURL)
-      .then((response) => {
-        this.raw_data = response.data          
-      });
-    },
-    formatData: function () {
-    },
     storeData: function (data) {
       let key = data.images.original_mp4.mp4
               
       // if the gif url doesn't already exist in the locally stored gifs, add it to the array of gifs
       // the need for this.keys needs to be refactored, has to be a way to loop through this.stored_gifs keys to not be redundant
       if (!this.keys.includes(key)) {
-        console.log('pushing data')
         this.displayed_gifs.push(data)
         this.stored_gifs[key] = data
         this.keys.push(key)
@@ -173,26 +175,29 @@ export default {
       this.random = this.search_string ? false : true
     },
     setDisplayedGif: function () {
-      console.log('setDisplayedGif')
-      console.log(this.displayed_gifs[this.index])
       this.display_gif = this.displayed_gifs[this.index]
     },
     cycle: function (operator) {
-      console.log('at cycle')
-      console.log(this.index)
       if (operator === '+' && this.index + 1 < this.displayed_gifs.length) {
         this.index++
       } else if (operator === '-' && this.index > 0) {
         this.index--
       }
-      console.log(this.index)
       this.setDisplayedGif()
+    },
+    copyToClipboard: function () {
+      if (navigator.clipboard.writeText(this.display_gif.images.original_mp4.mp4)) {
+        this.show_copied_message = true
+      }
     }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #333745;
+}
 input {
   margin: 0 .5rem;
   font-size: 16px;
@@ -202,95 +207,71 @@ input {
 button {
   margin: 0 .5rem;
   color: white;
-  background-color: #2c3e50;
-  /*width: 50px;*/
-  /*height: 25px;*/
-  border: none;
-  color: white;
+  background-color: #8457C5;
+  border-bottom: 2px solid #6A3BAD;
   padding: 5px 10px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
-  margin: 4px 2px;
   cursor: pointer;
-}
-iframe {
-  width: 200px;
-}
-video {
-  /*border: 1px black solid;
-  padding: 5px 5px;*/
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  margin-top: 3rem;
   min-height: 100vh;
   position: relative;
 }
-/*
-#page_container {
-  min-height: 100vh;
-  position: relative;
-}
-*/
 #input_container {
   margin-bottom: 2rem;
 }
-/* if default, width 30%, if mobile width 80% */
-.error_message_container {
+.message_parent_container {
   display: flex;
   justify-content: center;
+  height: 2.5rem;
+  margin: 2rem 0;
 }
-.error_message {
-  color: #DD3444;
-  border: 1px #DD3444 solid;
-  /*background-color: rgba(240, 240, 240);*/
-  border-radius: 3px;
-  width: 25%;
-  padding: 10px 0;
+.message_child_container {
+  width: 50%;
   font-size: 18px;
-}
-.error_message p {
-  padding: 0; 
-  margin: 0;
+  text-align: center;
 }
 @media only screen and (max-width: 800px) {
-  .error_message {
+  .message_child_container {
     width: 80%;
   }
 }
-
-.row {
+.error_message {
+  color: #DD3444;
+}
+.copied_message {
+  color: white;
+}
+.gif_parent_container {
   display: flex;
   justify-content: center;
-  /*border: 1px solid black;*/
+}
+.gif_container {
+  display: flex;
+  justify-content: center;
   margin: 4rem 0;
+  width: 500px;
+  background-color: #464C5D;
+  padding: 1rem 0;
+  border: 3px gray solid;
+  border-radius: 5px;
 }
-/*
-@media only screen and (max-width: 1245px) {
-  .row {
-    display: block;
-    margin-bottom: 4rem;
-  }
+.cycle_container {
+  display:flex; 
+  justify-content: space-between; 
+  margin-bottom: 1rem;
 }
-*/
-.five_col {
-  flex: 10%;
-  padding: 5px;
-  /*border: 1px red solid;*/
-}
-.three_col {
-  flex: 30%;
-  padding: 5px;
-  /*border: 1px blue solid;*/
-}
-.one_col {
-  /*width: 400px;*/
+.copy_to_clipboard_container {
+  margin-top: 1rem;
 }
 .loader {
   flex: 100%;
@@ -300,7 +281,7 @@ video {
   display: flex;
   justify-content: center;
   position: absolute;
-  bottom:50px;
+  bottom: 3rem;
   width: 100%;
 }
 @media only screen and (max-width: 1245px) {
